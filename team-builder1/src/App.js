39d8eb4ea4423c19f0ef96e3711d7c38482ad0e6
kayode <kayode.richards@gmail.com>
2020-10-13 17:03:47 -0400

    mvp finished having issues with axios

--- team-builder1/src/App.js
+++ team-builder1/src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
+import Member from './components/Member'
+import MemberForm from './components/MemberForm'
+// console.log(axios)
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: '',
+}
+
 
 function App() {
+  const [members, setMembers] = useState([])
+
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const updateForm = (inputName, inputValue) =>{
+    setFormValues({...formValues, [inputName]: inputValue,})
+  }
+
+  const submitForm = () =>{
+      let newMember ={
+        name: formValues.name.trim(),
+        email: formValues.email.trim(),
+        role: formValues.role,
+      }
+      if(!newMember.name || !newMember.email || !newMember.role) {
+        return
+      }
+      axios.post('fakeapi.com', newMember)
+      .then((response)=>{
+        setMembers([...members, response.data])
+        setFormValues(initialFormValues)
+      })
+      .catch(error =>{
+        console.log(error)
+      })
+  }
+  useEffect(()=>{
+    axios.get('fakeapi.com')
+    .then(response=>{
+      setMembers(response.data)
+    })
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= 'container'>
+      <h1>My Team Builder App</h1>
+    <MemberForm 
+    values={formValues} 
+    update={updateForm} 
+    submit={submitForm}
+    />
+
+    {members.map((member)=>{
+      return <Member key={member.id} details={member}/>
+    })}
     </div>
+    
+    
   );
 }
 
